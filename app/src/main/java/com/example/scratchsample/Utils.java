bdecdf4833a57f19d58d05ce6235ccf592a8c84e
Elvin <tmariaelvin@gmail.com>
2021-02-11 11:13:20 +0530

    Initial Commit
    Scratch Card like google pay reward card in android studio

---
+++ app/src/main/java/com/example/scratchsample/Utils.java
@@ -0,0 +1,27 @@
+package com.example.scratchsample;
+
+import android.content.Context;
+
+import java.util.Random;
+
+public class Utils {
+    static Random random = new Random();
+
+    public static float dipToPx(Context context, float dipValue) {
+        float density = context.getResources().getDisplayMetrics().density;
+        return dipValue * density;
+    }
+
+
+    //Generate random number(Prize)
+    private static String generateCodePart(int min, int max) {
+        int minNumber = 1;
+        int maxNumber = 100;
+        return String.valueOf((random.nextInt((maxNumber - minNumber) + 1) + minNumber));
+    }
+
+    public static String generateNewCode() {
+        String firstCodePart = generateCodePart(1000, 9999);
+        return "You Won\nRs." + firstCodePart;
+    }
+}
